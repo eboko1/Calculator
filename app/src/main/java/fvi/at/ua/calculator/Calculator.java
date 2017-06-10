7710ce4d619d6c07165b3b0d9280ca67f781003f
Victoria Frantsuh <eboko1@gmail.com>
2017-06-10 17:01:33 +0300

    update layout activity calculate

--- app/src/main/java/fvi/at/ua/calculator/Calculator.java
+++ app/src/main/java/fvi/at/ua/calculator/Calculator.java
@@ -18,39 +18,53 @@ public class Calculator extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
 
-        screen = (TextView)findViewById(R.id.textView);
-        screen.setText(display);
-    }
-
-    public void updateScreen(){
-        screen.setText(display);
-    }
-
-    public void onClickNumber(View view){
-        Button b = (Button) view;
-        display = display + b.getText();
-        updateScreen();
-    }
-
-    public void onClickOperator(View view){
-        Button b = (Button) view;
-        display = display + b.getText();
-        updateScreen();
     }
 
 
-    public void clear(){
-        display = "";
-        currentOperator = "";
-    }
 
-    public void onClickClear(View view){
-        clear();
-        updateScreen();
-    }
+<Button
+    android:id="@+id/button7"
+    android:layout_width="wrap_content"
+    android:layout_height="wrap_content"
+    android:background="@android:color/holo_purple"
+    android:onClick="onClickEquals"
+    android:text="7"
+    android:layout_weight="1"
+    android:textColor="@android:color/background_light"
+    android:textSize="30sp" />
 
-    public  void onClickEquals(View view){
+        <Button
+    android:id="@+id/button8"
+    android:layout_width="wrap_content"
+    android:layout_height="wrap_content"
+    android:background="@android:color/holo_purple"
+    android:onClick="onClickEquals"
+    android:text="8"
+    android:layout_weight="1"
+    android:textColor="@android:color/background_light"
+    android:textSize="30sp" />
 
-    }
+        <Button
+    android:id="@+id/button9"
+    android:layout_width="wrap_content"
+    android:layout_height="wrap_content"
+    android:background="@android:color/holo_purple"
+    android:onClick="onClickEquals"
+    android:text="9"
+    android:layout_weight="1"
+    android:textColor="@android:color/background_light"
+    android:textSize="30sp" />
+
+        <Button
+    android:id="@+id/buttonMyltip"
+    android:layout_width="wrap_content"
+    android:layout_height="wrap_content"
+    android:layout_weight="1"
+    android:background="@android:color/holo_purple"
+    android:onClick="onClickClear"
+    android:text=""
+    android:textColor="@android:color/background_light"
+    android:textSize="30sp" />
+
 
 }
