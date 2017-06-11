515f7c8aa0f669aa6fad149695df830000576c19
Victoria Frantsuh <eboko1@gmail.com>
2017-06-11 13:23:21 +0300

    update

--- app/src/main/java/fvi/at/ua/calculator/Calculator.java
+++ app/src/main/java/fvi/at/ua/calculator/Calculator.java
@@ -1,7 +1,5 @@
 package fvi.at.ua.calculator;
 
-import android.content.Intent;
-import android.media.MediaCodec;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -9,9 +7,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.StringTokenizer;
-import java.util.regex.Pattern;
 
 
 public class Calculator extends AppCompatActivity {
